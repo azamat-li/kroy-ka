f27aa1bcc754b01375fc86cab0a2477ad393d105
azamat-li <azamatalifullstack@gmail.com>
2021-02-23 17:37:13 +0500

    feat: delete, version 0.2

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -16,7 +16,7 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
     },
     include: {
       author: {
-        select: { name: true },
+        select: { name: true, email: true },
       },
     },
   })
@@ -30,6 +30,13 @@ async function publishPost(id: number): Promise<void> {
     method: 'PUT',
   })
   await Router.push('/')
+}
+
+async function deletePost(id: number): Promise<void> {
+  await fetch(`http://localhost:3000/api/post/${id}`, {
+    method: 'DELETE',
+  })
+  Router.push('/')
 }
 
 const Post: React.FC<PostProps> = (props) => {
@@ -53,6 +60,10 @@ const Post: React.FC<PostProps> = (props) => {
         {!props.published && userHasValidSession && postBelongsToUser && (
           <button onClick={() => publishPost(props.id)}>Publish</button>
         )}
+				{
+				userHasValidSession && postBelongsToUser && (
+				<button onClick={() => deletePost(props.id)}>Delete</button>
+				)}
       </div>
       <style jsx>{`
         .page {
